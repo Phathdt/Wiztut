5d00c6a5ff246b3dbaacbd10d2d4bf68b4b9fd7c
Phathdt <Phathdt.leviateam@gmail.com>
2017-09-23 12:52:14 +0700

    add conversation and message

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ApplicationRecord
   has_many :courses_as_teachers, class_name: "Course", foreign_key: "teacher_id"
   has_many :courses_as_students, class_name: "Course", foreign_key: "student_id"
   has_many :ratings, class_name: "Rating", foreign_key: "rated_id"
+
+  has_many :conversations, :foreign_key => :sender_id
   # some scope user
   default_scope { where(active: true) }
 end
