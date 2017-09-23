1f07351f3aad55f11681597b6d1a497208063b16
Phathdt <Phathdt.leviateam@gmail.com>
2017-09-23 11:12:48 +0700

    add seed file and add grade and subject to profile

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
 User.create(email: "Ps@gma.com" , password: "123123", password_confirmation: "123123")
-params = { dob: Date.current , sex: 0 , school:"UIT" , degree: 0, graduation_year: 2018, salary: 1000, about_me: "lorem isbum la bum", phone: "0945804675"}
-User.first.build_profile(params)
+params = { dob: Date.current , sex: 0 , school:"UIT" , degree: 0, graduation_year: 2018, salary: 1000, grades: [1], subjects: ["Toan"], about_me: "lorem isbum la bum", phone: "0945804675"}
+User.first.build_profile(params).save
