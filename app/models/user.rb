8a97779a0c306cdb0b9b70ac093f822aee1e4e1f
Phathdt <Phathdt.leviateam@gmail.com>
2017-09-23 09:30:15 +0700

    add attr Admin, active and teacher to User add default scope what user active -> true

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   extend Devise::Models
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  # some scope user
+  default_scope where( active: true)
 end
