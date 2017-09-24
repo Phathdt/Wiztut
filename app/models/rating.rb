66bcd86dd5f58a00991f7d09049b9f4999ee5ccf
Phathdt <Phathdt.leviateam@gmail.com>
2017-09-25 01:59:40 +0700

    add calculate rating after create rating

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,5 +1,12 @@
 class Rating < ApplicationRecord
+  after_save :calculate_user_rate
   belongs_to :rater , class_name: "User", foreign_key: "rater_id"
   belongs_to :rated , class_name: "User", foreign_key: "rated_id"
   validates_uniqueness_of :rater_id, scope: [:rated_id]
+
+  def calculate_user_rate
+    user = self.rated
+    rate = user.ratings.sum(:rate).to_f / user.ratings.count
+    user.update(rate: rate)
+  end
 end
--- db/migrate/20170924164215_add_rate_to_user.rb
+++ db/migrate/20170924164215_add_rate_to_user.rb
@@ -1,5 +1,5 @@
 class AddRateToUser < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :rate, :float, precision: 5, scale: 2, default: 0
+    add_column :users, :rate, :decimal, precision: 5, scale: 2, default: 0
   end
 end
