35573b64019bc2d53d5a2d5290faa736d9c9e8df
Phathdt <Phathdt.leviateam@gmail.com>
2017-09-25 14:27:09 +0700

    add permission for rating, only student could rating and destroy it, teacher can not destroy rating

--- app/controllers/api/v1/ratings_controller.rb
+++ app/controllers/api/v1/ratings_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::RatingsController < Api::V1::BaseApiController
   before_action :find_rating, only: [:destroy]
   before_action :authenticate_request!, except: :index
-
-  # còn chưa làm logic cho rating , cần viết thêm service rating permission
+  before_action :check_permission_rate, only: [ :create, :destroy]
+  
   def index
     ratings = User.joins(:rateds).joins(:profile).where("ratings.id IN (?)", User.find(params[:profile_id]).rating_ids)
-      .select('users.id,profiles.name, users.email, ratings.comment')
-      .order("ratings.updated_at DESC").page(params[:page])
+    .select('users.id,profiles.name, users.email, ratings.comment')
+    .order("ratings.updated_at DESC").page(params[:page])
 
     render json: { ratings: ratings }, status: 200
   end
@@ -15,7 +15,7 @@ class Api::V1::RatingsController < Api::V1::BaseApiController
     # delete old rating if exist
     old_rating = Rating.find_by(rater_id: strong_params[:rater_id], rated_id: strong_params[:rated_id])
     old_rating.destroy if old_rating.nil?
-    
+
     rating = Rating.new(strong_params)
     if rating.save
       render json: {
@@ -34,9 +34,16 @@ class Api::V1::RatingsController < Api::V1::BaseApiController
 
   private
 
+  def check_permission_rate
+    unless current_user.courses_as_students.pluck(:teacher_id).include?(strong_params[:rater_id])
+      render json: { message: I18n.t('.dont_have_permission') }, status: 406
+    end
+  end
+
   def find_rating
     @rating = Rating.find(params[:id])
   end
+
   def strong_params
     params.require(:ratings).permit( :rater_id, :rated_id, :rate, :comment)
   end
