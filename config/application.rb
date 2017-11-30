a645d28ee1f442a01fe9dfa6a25374202c0616b3
Phathdt <Phathdt.leviateam@gmail.com>
2017-11-30 09:59:46 +0700

    handle CatchJsonParseErrors

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require_relative 'boot'
 
 require 'rails/all'
 
+require "./lib/middleware/catch_json_parse_errors.rb"
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -10,6 +12,7 @@ module Wiztut
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.middleware.insert_before Rack::Head, CatchJsonParseErrors
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
