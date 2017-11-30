82852de24c43de9f84197ea5b405530162e4b7c8
Phathdt <Phathdt.leviateam@gmail.com>
2017-11-30 17:09:07 +0700

    change status course

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,7 +2,7 @@ class Course < ApplicationRecord
   belongs_to :teacher , foreign_key: "teacher_id", class_name: 'User'
   belongs_to :student , foreign_key: "student_id", class_name: 'User'
   validates_uniqueness_of :teacher_id, scope: [:student_id]
-  enum status:  %i(waiting_brand_approval success canceled)
+  enum status:  %i(waiting_teacher_approval success canceled)
 
   scope :get_your_course, -> (user_id) {
     where("teacher_id = ? OR student_id = ?",user_id, user_id)
