b51e274bf02131e1ebada7fbdaf78aa88987e02b
Phathdt <Phathdt.leviateam@gmail.com>
2017-11-30 10:00:31 +0700

    add validate to cp and order by create

--- app/models/course_post.rb
+++ app/models/course_post.rb
@@ -1,6 +1,11 @@
 class CoursePost < ApplicationRecord
   belongs_to :user, optional: true
 
+  validates :title, presence: true
+  validates :grade, :subject, :time, :address, :salary, :frequency , presence: true, numericality: { only_integer: true }
+
+  scope :active, -> { where(status: true) }
+
   def search(search_params)
     CoursePost.where(search_params)
   end
