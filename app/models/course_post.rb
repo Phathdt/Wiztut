8ce6b576c0742cf47dcd1494cd839c0fee1ea20e
Phathdt <Phathdt.leviateam@gmail.com>
2017-12-20 19:14:53 +0700

    remove validate salary

--- app/models/course_post.rb
+++ app/models/course_post.rb
@@ -3,7 +3,7 @@ class CoursePost < ApplicationRecord
   has_many :profile, through: :user
 
   validates :title, presence: true
-  validates :grade, :subject, :time, :address, :salary, :frequency , presence: true, numericality: { only_integer: true }
+  validates :grade, :subject, :time, :address, :frequency , presence: true, numericality: { only_integer: true }
 
   scope :active, -> { where(status: true) }
 
