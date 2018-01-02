d7030f3fec0910d8b8c80a2deac4526a73272e7d
Phathdt <Phathdt.leviateam@gmail.com>
2018-01-02 23:08:56 +0700

    change rating

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,14 +1,7 @@
 class Rating < ApplicationRecord
-  after_save :calculate_user_rate
   belongs_to :rater , class_name: "User", foreign_key: "rater_id"
   belongs_to :rated , class_name: "User", foreign_key: "rated_id"
 
   validates_uniqueness_of :rater_id, scope: [:rated_id]
-  validates :rate, presence: true, inclusion: { in: [0, 5] }
-
-  def calculate_user_rate
-    user = self.rated
-    rate = user.ratings.sum(:rate).to_f / user.ratings.count
-    user.update(rate: rate)
-  end
+  validates :rate, presence: true, inclusion: { in: 0..5 }
 end
